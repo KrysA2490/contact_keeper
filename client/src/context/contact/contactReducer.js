8d71e9923c76329c55012e50b32ac75d87ca9b63
Elijah Adeoye <Temiwale88@gmail.com>
2021-06-16 21:23:47 -0700

    Update Contact

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -31,7 +31,7 @@ export default (state, action) => {
             console.log('updating contact')
             return{
                 ...state,
-                contacts: state.contacts.map(contact => contact.id === action.payload.id? action.payload: contact),
+                contacts: state.contacts.map(contact => contact._id === action.payload._id? action.payload: contact),
                 loading: false     
             }
         case DELETE_CONTACT:
