c0920072c7fcb31df02d7dea1a9760066e97c173
Elijah Adeoye <Temiwale88@gmail.com>
2021-06-16 21:00:36 -0700

    Get & Clear Contacts

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,24 +1,28 @@
 import React, {Fragment, useContext, useEffect} from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ContactItem from './ContactItem';
+import Spinner from '../layout/Spinner';
 import ContactContext from '../../context/contact/contactContext';
 
 
 const Contacts = () => {
     const contactContext = useContext(ContactContext)
 
-    const{contacts, filtered } = contactContext;
+    const{contacts, filtered, getContacts, loading } = contactContext;
 
     useEffect(() => {
-        console.log(contacts)
-    })
+        getContacts();
+        // eslint-disable-next-line
+    }, [])
 
-    if(contacts.length === 0) {
+    if(contacts !== null && contacts.length === 0 && !loading) {
         return <h4>Please add a contact</h4>
     }
 
     return (
         <Fragment>
+            
+            {contacts !== null && !loading ? (
             <TransitionGroup> 
             {/* If something in filtered, show that. If nothing filtered show contacts */}
             {
@@ -42,6 +46,8 @@ const Contacts = () => {
                 ))
             }
             </TransitionGroup>
+            // Show spinner if contacts are loading
+            ) : <Spinner/>}
         </Fragment>
     )
 }
